96874606eb3b415fd67f211d30893222e036ff38
yanushevilya <yanushev.ilya@gmail.com>
2021-02-07 17:48:20 +0200

    2

--- app/src/main/java/com/example/atbactions/DbHandler.java
+++ app/src/main/java/com/example/atbactions/DbHandler.java
@@ -1,88 +1,137 @@
 package com.example.atbactions;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.SQLException;
+
+import android.content.Context;
 import android.database.sqlite.*;
+import androidx.annotation.Nullable;
 
-public class DbHandler {
-    // Константа, в которой хранится адрес подключения
-    private static final String CON_STR = "jdbc:sqlite:C:\\Users\\yanus\\IdeaProjects\\testboard\\atb_db.s3db";
+public class DbHandler extends SQLiteOpenHelper {
+    // поля базы данных
+    public final static int DATABASE_VERSION = 1;
+    public final static String DATABASE_NAME = "ATBProductsDB";
+    public final static String TABLE_PRODUCTS = "ATBProductsTable";
+    public final static String TABLE_LINKS = "ATBLinksTable";
 
-    // Используем шаблон одиночка, чтобы не плодить множество
-    // экземпляров класса DbHandler
-    private static DbHandler instance = null;
+    // поля таблицы ATBProductsTable
+    public final static String KEY_ID = "_id";
+    public final static String KEY_CODE = "code";
+    public final static String KEY_NAME = "name";
+    public final static String KEY_IMAGE = "image";
+    public final static String KEY_COAST = "coast";
 
-    public static synchronized DbHandler getInstance() throws SQLException {
-        if (instance == null)
-            instance = new DbHandler();
-        return instance;
+    // поля таблицы ATBLinksTable
+    public final static String KEY_LINK = "link";
+
+    // конструктор
+    public DbHandler(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    // Объект, в котором будет храниться соединение с БД
-    private Connection connection;
+    // сработает при создании базы данных (вызывается только если БД не существует и ее надо создавать)
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_PRODUCTS + "(" + KEY_ID + " integer primary key, " + KEY_CODE +
+                " integer, " + KEY_NAME + " text, " + KEY_IMAGE + " text, " + KEY_COAST + " integer)");
+        db.execSQL("create table " + TABLE_LINKS + "(" + KEY_ID + " integer primary key, " + KEY_LINK + " text)");
 
-    private DbHandler() throws SQLException {
-        // Регистрируем драйвер, с которым будем работать
-        // в нашем случае Sqlite
-        DriverManager.registerDriver(new JDBC());
-        // Выполняем подключение к базе данных
-        this.connection = DriverManager.getConnection(CON_STR);
+
     }
 
-//    public List<Product> getAllProducts() {
+    // сработает при изменении номера версии DATABASE_VERSION
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // для обновления структуры таблицы БД можно сначала уничтожить существующую таблицу
+        db.execSQL("drop table if exists " + TABLE_PRODUCTS);
+        // после чего вызвать метод onCreate для создания новой версии таблицы с обновленной структурой
+        onCreate(db);
+
+    }
+
+
+//    // Константа, в которой хранится адрес подключения
+//    private static final String CON_STR = "jdbc:sqlite:C:\\Users\\yanus\\IdeaProjects\\testboard\\atb_db.s3db";
 //
-//        // Statement используется для того, чтобы выполнить sql-запрос
-//        try (Statement statement = this.connection.createStatement()) {
-//            // В данный список будем загружать наши продукты, полученные из БД
-//            List<Product> products = new ArrayList<Product>();
-//            // В resultSet будет храниться результат нашего запроса,
-//            // который выполняется командой statement.executeQuery()
-//            ResultSet resultSet = statement.executeQuery("SELECT id, good, price, category_name FROM products");
-//            // Проходимся по нашему resultSet и заносим данные в products
-//            while (resultSet.next()) {
-//                products.add(new Product(resultSet.getInt("id"),
-//                        resultSet.getString("good"),
-//                        resultSet.getDouble("price"),
-//                        resultSet.getString("category_name")));
-//            }
-//            // Возвращаем наш список
-//            return products;
+//    // Используем шаблон одиночка, чтобы не плодить множество
+//    // экземпляров класса DbHandler
+//    private static DbHandler instance = null;
 //
+//
+//
+//    public static synchronized DbHandler getInstance() throws SQLException {
+//        if (instance == null)
+//            instance = new DbHandler();
+//        return instance;
+//    }
+//
+//    // Объект, в котором будет храниться соединение с БД
+//    private Connection connection;
+//
+//    private DbHandler() throws SQLException {
+//        // Регистрируем драйвер, с которым будем работать
+//        // в нашем случае Sqlite
+//        DriverManager.registerDriver(new JDBC());
+//        // Выполняем подключение к базе данных
+//        this.connection = DriverManager.getConnection(CON_STR);
+//    }
+//
+////    public List<Product> getAllProducts() {
+////
+////        // Statement используется для того, чтобы выполнить sql-запрос
+////        try (Statement statement = this.connection.createStatement()) {
+////            // В данный список будем загружать наши продукты, полученные из БД
+////            List<Product> products = new ArrayList<Product>();
+////            // В resultSet будет храниться результат нашего запроса,
+////            // который выполняется командой statement.executeQuery()
+////            ResultSet resultSet = statement.executeQuery("SELECT id, good, price, category_name FROM products");
+////            // Проходимся по нашему resultSet и заносим данные в products
+////            while (resultSet.next()) {
+////                products.add(new Product(resultSet.getInt("id"),
+////                        resultSet.getString("good"),
+////                        resultSet.getDouble("price"),
+////                        resultSet.getString("category_name")));
+////            }
+////            // Возвращаем наш список
+////            return products;
+////
+////        } catch (SQLException e) {
+////            e.printStackTrace();
+////            // Если произошла ошибка - возвращаем пустую коллекцию
+////            return Collections.emptyList();
+////        }
+////    }
+//
+//    // Добавление продукта в БД
+//    public void addProduct() {
+//        // Создадим подготовленное выражение, чтобы избежать SQL-инъекций
+//        try (PreparedStatement statement = this.connection.prepareStatement(
+//
+//                "INSERT INTO Product(`name`, `image`, `coast`) " +
+//                        "VALUES(?, ?, ?)")) {
+//            statement.setObject(1, "Суши");
+//            statement.setObject(2, "Японская кухня");
+//            statement.setObject(3, 11400);
+//            // Выполняем запрос
+//            statement.execute();
 //        } catch (SQLException e) {
 //            e.printStackTrace();
-//            // Если произошла ошибка - возвращаем пустую коллекцию
-//            return Collections.emptyList();
 //        }
 //    }
+//
+//    // Удаление продукта по id
+//    public void deleteProduct(int id) {
+//        try (PreparedStatement statement = this.connection.prepareStatement(
+//                "DELETE FROM Products WHERE id = ?")) {
+//            statement.setObject(1, id);
+//            // Выполняем запрос
+//            statement.execute();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
 
-    // Добавление продукта в БД
-    public void addProduct() {
-        // Создадим подготовленное выражение, чтобы избежать SQL-инъекций
-        try (PreparedStatement statement = this.connection.prepareStatement(
-
-                "INSERT INTO Product(`name`, `image`, `coast`) " +
-                        "VALUES(?, ?, ?)")) {
-            statement.setObject(1, "Суши");
-            statement.setObject(2, "Японская кухня");
-            statement.setObject(3, 11400);
-            // Выполняем запрос
-            statement.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Удаление продукта по id
-    public void deleteProduct(int id) {
-        try (PreparedStatement statement = this.connection.prepareStatement(
-                "DELETE FROM Products WHERE id = ?")) {
-            statement.setObject(1, id);
-            // Выполняем запрос
-            statement.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 }
--- app/src/main/java/com/example/atbactions/MainActivity.java
+++ app/src/main/java/com/example/atbactions/MainActivity.java
@@ -12,48 +12,54 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Set;
 import java.util.TreeSet;
+//import static com.example.atbactions.DbHandler.getInstance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-import static com.example.atbactions.DbHandler.getInstance;
 
-public class MainActivity extends AppCompatActivity {
 
-    TextView tv1 = findViewById(R.id.tv1);
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    DbHandler dbHandler;
+
+//    TextView tv1 = findViewById(R.id.tv1);
+//
     // локация
     String location = "17";
     // исходная ссылка HTML
     String html = "https://zakaz.atbmarket.com/";
-    // сессия id в cookies
-    String sessionId;
+//    // сессия id в cookies
+//    String sessionId;
     // шаблоны на выборку из HTML
     String catalogLinks = "[href*=catalog/" + location + "]"; //ссылки на все подкатегории товаров
     String paginationLinks = "ul[class=pagination] a[class=page-link]"; // подстраницы
-
-    String productImg = "div[class=img-wrapper] img"; // фото товара
-//    <img src="https://src.zakaz.atbmarket.com/cache/photos/catalog_list_3614.jpg" class="img-fluid blur-up lazyload
-//    bg-img" alt="Корм 85г Gourmet Gold консерви для дорослих котів ніжні биточки з індічкою та шпинатом,">
-
-    String productPrice = "span[class=price]"; // цена товара
-//    <span class="price">
-//            1540
-
-    String productName = "div[class=product-detail text-center]"; // название товара
-//    <div class="product-detail text-center">
-//    Корм 85г Gourmet Gold консерви для дорослих котів ніжні биточки з індічкою та шпинатом, 1 шт
-
-    String productCode = "div[class=product-detail text-center] a[href]"; // ссылка с кодом товара
-//    <a href="/product/64/136257">
-//    Корм 85г Gourmet Gold консерви для дорослих котів ніжні биточки з індічкою та шпинатом,
-
-    BufferedWriter bw;
-
-    // список всех ссылок на все товары в локации
-    Set<String> linksOnProduct = new TreeSet<>();
+//
+//    String productImg = "div[class=img-wrapper] img"; // фото товара
+////    <img src="https://src.zakaz.atbmarket.com/cache/photos/catalog_list_3614.jpg" class="img-fluid blur-up lazyload
+////    bg-img" alt="Корм 85г Gourmet Gold консерви для дорослих котів ніжні биточки з індічкою та шпинатом,">
+//
+//    String productPrice = "span[class=price]"; // цена товара
+////    <span class="price">
+////            1540
+//
+//    String productName = "div[class=product-detail text-center]"; // название товара
+////    <div class="product-detail text-center">
+////    Корм 85г Gourmet Gold консерви для дорослих котів ніжні биточки з індічкою та шпинатом, 1 шт
+//
+//    String productCode = "div[class=product-detail text-center] a[href]"; // ссылка с кодом товара
+////    <a href="/product/64/136257">
+////    Корм 85г Gourmet Gold консерви для дорослих котів ніжні биточки з індічкою та шпинатом,
+//
+//    BufferedWriter bw;
+//
+//    // список всех ссылок на все товары в локации
+//    Set<String> linksOnProduct = new TreeSet<>();
 
 
     @Override
@@ -61,39 +67,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MainActivity appMain = new MainActivity();
-//        DbWork.Conn();
-//        DbWork.CreateDB();
-//        DbWork.WriteDB();
-        // Создаем экземпляр по работе с БД
-        DbHandler dbHandler = null;
-        try {
-            dbHandler = getInstance();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-
-        // получаем ссылки на страницы всех подкатегорий товаров
-        Document document = appMain.getCookies(appMain.html+appMain.location);
-        Elements links = document.getElementsByTag("a");
-        links = links.select(appMain.catalogLinks);
-//        for (Element l: links) {
-//            System.out.println(l.absUrl("href"));
-//        }
-
-        // получаем ссылки на подстраницы всех подкатегорий товаров (страницы пагинации)
-        for (Element el: links) {
-            Document documentPagination = appMain.getCookies(el.absUrl("href")); // ложим каждую ссылку в циклн
-            Elements linksPagination = documentPagination.select(appMain.paginationLinks);
-            for (Element l: linksPagination) {
-                System.out.println(l.absUrl("href"));
-                appMain.saveDataOnStorage(l.absUrl("href") + "\n");
-                // Добавляем запись
-                dbHandler.addProduct();
-//                System.out.println(l.text());
-            }
-        }
+        dbHandler = new DbHandler(this);
 
     }
 
@@ -114,18 +88,77 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // запись извлеченных данных в файл
-    public void saveDataOnStorage (String se){
-        try {
-            bw = new BufferedWriter(new FileWriter("atb_links.txt", true));
-            bw.write(se);
-            bw.flush();
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+//    public void saveDataOnStorage (String se){
+//        try {
+//            bw = new BufferedWriter(new FileWriter("atb_links.txt", true));
+//            bw.write(se);
+//            bw.flush();
+//            bw.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    @Override
+    public void onClick(View v) {
+        // SQLiteDatabase - предназначен для управления БД SQLite и имеет в себе методы
+        // query(), insert(), delete(), update(), а также execSQL, который позволяет выполнить любой код на языке SQL
+        SQLiteDatabase sqLiteDatabase = dbHandler.getWritableDatabase(); // getWritableDatabase() возвращает instance БД
+
+        // ContentValues используется для добавления новых строк в таблицу
+        // каждый объект этого класса выглядит как массив с именами столбцов и значениями, которые им соответствуют
+        // | _id | code | name | image | coast |
+        // |  1  | 2344 | суши | .img3 | 14400 |
+        ContentValues contentValues = new ContentValues();
+
+        switch (v.getId()) {
+            case R.id.btn_addProduct:
+
+//                // получаем ссылки на страницы всех подкатегорий товаров
+//                Document document = getCookies(html+location);
+//                Elements links = document.getElementsByTag("a");
+//                links = links.select(catalogLinks);
+//]
+//                // получаем ссылки на подстраницы всех подкатегорий товаров (страницы пагинации)
+//                for (Element el: links) {
+//                    Document documentPagination = getCookies(el.absUrl("href")); // ложим каждую ссылку в цикл
+//                    Elements linksPagination = documentPagination.select(paginationLinks);
+//                    for (Element l: linksPagination) {
+//                        System.out.println(l.absUrl("href"));
+//                        // Добавляем запись
+//                        contentValues.put(DbHandler.KEY_CODE, code);
+//                        contentValues.put(DbHandler.KEY_NAME, name);
+//                        contentValues.put(DbHandler.KEY_IMAGE, image);
+//                        contentValues.put(DbHandler.KEY_COAST, coast);
+//                    }
+//                }
+            case R.id.btn_addAllLinks:
+                // получаем ссылки на страницы всех подкатегорий товаров
+                Document document = getCookies(html+location);
+                Elements links = document.getElementsByTag("a");
+                links = links.select(catalogLinks);
+//                for (Element l: links) {
+//                    System.out.println(l.absUrl("href"));
+//                }
+
+                // получаем ссылки на подстраницы всех подкатегорий товаров (страницы пагинации)
+                for (Element el: links) {
+                    Document documentPagination = getCookies(el.absUrl("href")); // ложим каждую ссылку в цикл
+                    Elements linksPagination = documentPagination.select(paginationLinks);
+                    for (Element l: linksPagination) {
+                        System.out.println(l.absUrl("href"));
+//                     saveDataOnStorage(l.absUrl("href") + "\n"); // запись ссылок в файл
+                        // Добавляем запись
+                        contentValues.put(DbHandler.KEY_LINK, l.absUrl("href"));
+                    }
+                }
+
+
         }
     }
 
-    // () получение списка всех ссылок на все товары в локации
-
-    // () перебор списка ссылок на все товары локациии с извлечением из них данных
+//
+//    // () получение списка всех ссылок на все товары в локации
+//
+//    // () перебор списка ссылок на все товары локациии с извлечением из них данных
 }
